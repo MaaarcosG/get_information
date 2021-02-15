3b8a1ba52a97035858d8b49fe7feaa321365c2bf
MaaarcosG <gut17909@uvg.edu.gt>
2021-02-15 14:09:35 -0600

    update

--- information.py
+++ information.py
@@ -7,6 +7,11 @@ import os
 def get_size(bytes, suffix="B"):
     pass
 
+def bytes_to_gbytes(bytes):
+    gb = bytes/(1024*1024*1024)
+    gb = round(gb,2)
+    return gb
+
 # DATOS DE LA COMPUTADORA 
 
 perfil_information = [
@@ -19,28 +24,41 @@ perfil_information = [
     'platform',
     'processor'
 ]
-
+print(' ')
 print("="*40, "System Information", "="*40)
 for i in perfil_information:
     if hasattr(pl, i):
         print('[+] %s: %s' % (i, getattr(pl,i)()))
 
-print ("Number of CPUs: " +str(psutil.cpu_count()))
-print ("Number of Physical CPUs: " +str(psutil.cpu_count(logical=False)))
+print(' ')
+print("="*40, "CPU Information", "="*40)
+print ("[+] Number of CPUs: %s " % str(psutil.cpu_count()))
+print ("[+] Number of Physical CPUs: %s " % str(psutil.cpu_count(logical=False)))
 
-def getMachine_addr():
-    """ Find OS and run appropriate read mobo serial num command"""
-    os_type = sys.platform.lower()
- 
-    if "win" in os_type:
-        command = "wmic bios get serialnumber"
- 
-    elif "linux" in os_type:
-        command = "hal-get-property --udi /org/freedesktop/"  \
-        "Hal/devices/computer --key system.hardware.uuid"
- 
-    elif "darwin" in os_type:
-        command = "ioreg -l | grep IOPlatformSerialNumber"
-    return os.popen(command).read().replace("\n", "").replace("  ", "").replace(" ", "")
- 
-print("Your motherboard", getMachine_addr())
+with open("/proc/cpuinfo", "r") as f:
+    file_info = f.readlines()
+
+cpuinfo = [x.strip().split(":")[1] for x in file_info if "model name" in x]
+for index, item in enumerate(cpuinfo):
+    print("[+] Processor: %s: %s" % (str(index), item))
+
+
+print(' ')
+print("="*40, "Disk Information", "="*40)
+disk_partitions = psutil.disk_partitions()
+for partition in disk_partitions:
+    print("[+] Partition Device: %s" % partition.device)
+    print("[+] File System: %s" % partition.fstype)
+    print("[+] Mountpoint: %s " % partition.mountpoint)
+
+    print('Disk Usage')
+    disk_usage = psutil.disk_usage(partition.mountpoint)
+    print("[+] Total Disk Space : %d GB" % bytes_to_gbytes(disk_usage.total))
+    print("[+] Free Disk Space %d GB" % bytes_to_gbytes(disk_usage.free))
+    print("[+] Used Disk Space %d GB" % bytes_to_gbytes(disk_usage.used))
+    print("[+] Percentage Used %d %s" % (disk_usage.percent, '%'))
+
+disk_rw = psutil.disk_io_counters()
+print(f"[+] Total Read since boot : {bytes_to_gbytes(disk_rw.read_bytes)} GB")
+print(f"[+] Total Write sice boot : {bytes_to_gbytes(disk_rw.write_bytes)} GB")
+
